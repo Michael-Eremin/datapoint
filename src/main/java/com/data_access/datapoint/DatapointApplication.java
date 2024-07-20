258acaed30a7e21c497ee8d155a34157d93469fc
MichaelErem <ereminmichael01@gmail.com>
2024-07-20 17:41:39 +0300

    Initial commit

---
+++ src/main/java/com/data_access/datapoint/DatapointApplication.java
@@ -0,0 +1,13 @@
+package com.data_access.datapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data_access/datapoint/DatapointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_access.datapoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
